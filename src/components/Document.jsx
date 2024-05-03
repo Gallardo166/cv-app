9ba6a63ce70700b7cef4dcbc54f3dba09159ca72
Gallardo Lee <leegallardo16@gmail.com>
2024-05-03 15:31:40 +0700

    Style document layout

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,35 +1,65 @@
+import "../styles/Document.css";
+
 export default function Document({ personalDetails, education, experience }) {
   return (
     <div className="document">
       <h1>{personalDetails.fullName}</h1>
-      <h3>{personalDetails.phoneNumber}</h3>
-      <h3>{personalDetails.email}</h3>
-      {personalDetails.linkedIn ? <h3>{personalDetails.linkedIn}</h3> : null}
-      {personalDetails.gitHub ? <h3>{personalDetails.gitHub}</h3> : null}
-      {education.map((education) => (
-        <div key={education.id}>
-          <h3>{education.institution}</h3>
-          <h3>{education.degree}</h3>
-          <h3>{education.startDate + " - " + education.endDate}</h3>
-        </div>
-      ))}
-      {experience.root.childIds.map((experienceId) => (
-        <div key={experienceId}>
-          <h3>{experience[experienceId].organization}</h3>
-          <ul>
-            {experience[experienceId].childIds.map((contributionId) => (
-              <li key={contributionId}>
-                {experience[contributionId].contribution}
-              </li>
-            ))}
-          </ul>
-          <h3>
-            {experience[experienceId].startDate +
-              " - " +
-              experience[experienceId].endDate}
-          </h3>
-        </div>
-      ))}
+      <div className="details">
+        <h3>{personalDetails.phoneNumber}</h3>
+        <h2>|</h2>
+        <h3>{personalDetails.email}</h3>
+        {personalDetails.linkedIn ? (
+          <div>
+            <h2>|</h2>
+            <h3>{personalDetails.linkedIn}</h3>
+          </div>
+        ) : null}
+        {personalDetails.gitHub ? (
+          <div>
+            <h2>|</h2>
+            <h3>{personalDetails.gitHub}</h3>
+          </div>
+        ) : null}
+      </div>
+      <div className="education">
+        <h2>Education</h2>
+        <hr></hr>
+        {education.map((education) => (
+          <div key={education.id}>
+            <h3>{education.institution}</h3>
+            <h4>{education.location}</h4>
+            <h5>{education.degree}</h5>
+            <h5 className="date">
+              {education.startDate +
+                (education.endDate ? " - " + education.endDate : "")}
+            </h5>
+          </div>
+        ))}
+      </div>
+      <div className="experience">
+        <h2>Experience</h2>
+        <hr></hr>
+        {experience.root.childIds.map((experienceId) => (
+          <div key={experienceId}>
+            <h3>{experience[experienceId].organization}</h3>
+            <h4>
+              {experience[experienceId].startDate +
+                (experience[experienceId].endDate
+                  ? " - " + experience[experienceId].endDate
+                  : "")}
+            </h4>
+            <h5>{experience[experienceId].position}</h5>
+            <h5 className="location">{experience[experienceId].location}</h5>
+            <ul>
+              {experience[experienceId].childIds.map((contributionId) => (
+                <li key={contributionId}>
+                  {experience[contributionId].contribution}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
-export default function PersonalDetails({ personalDetails, handleChangePersonalDetails }) {
+export default function PersonalDetails({
+  personalDetails,
+  handleChangePersonalDetails,
+}) {
   const [isShown, setIsShown] = useState(true);
 
   function handleClick() {
@@ -9,62 +12,68 @@ export default function PersonalDetails({ personalDetails, handleChangePersonalD
 
   return (
     <section>
-      <div
-        className='header'
-      >
+      <div className="header">
         <h2>Personal Details</h2>
         <button onClick={handleClick}>&#8964;</button>
       </div>
-      <div
-        className={'personal-details-inputs' + (!isShown ? ' hidden' : '')}
-      >
-        <label htmlFor='full-name'>Full Name <span className="required">required</span></label>
-        <input 
-          id='full-name' 
-          type='text'
+      <div className={"personal-details-inputs" + (!isShown ? " hidden" : "")}>
+        <label htmlFor="full-name">
+          Full Name <span className="required">required</span>
+        </label>
+        <input
+          id="full-name"
+          type="text"
           value={personalDetails.fullName}
           onChange={(e) => {
-            handleChangePersonalDetails('fullName', e.target.value);
+            handleChangePersonalDetails("fullName", e.target.value);
           }}
         />
-        <label htmlFor='phone-number'>Phone Number <span className="required">required</span></label>
-        <input 
-          id='phone-number' 
-          type='number' 
+        <label htmlFor="phone-number">
+          Phone Number <span className="required">required</span>
+        </label>
+        <input
+          id="phone-number"
+          type="text"
           value={personalDetails.phoneNumber}
           onChange={(e) => {
-            handleChangePersonalDetails('phoneNumber', e.target.value);
+            handleChangePersonalDetails("phoneNumber", e.target.value);
           }}
         />
-        <label htmlFor='email'>Email <span className="required">required</span></label>
-        <input 
-          id='email' 
-          type='email' 
+        <label htmlFor="email">
+          Email <span className="required">required</span>
+        </label>
+        <input
+          id="email"
+          type="email"
           value={personalDetails.email}
           onChange={(e) => {
-            if (e.target.value === '') return;
-            handleChangePersonalDetails('email', e.target.value);
+            if (e.target.value === "") return;
+            handleChangePersonalDetails("email", e.target.value);
           }}
         />
-        <label htmlFor='linkedin'>LinkedIn Link <span className="recommended">recommended</span></label>
-        <input 
-          id='linkedin' 
-          type='text' 
+        <label htmlFor="linkedin">
+          LinkedIn Link <span className="recommended">recommended</span>
+        </label>
+        <input
+          id="linkedin"
+          type="text"
           value={personalDetails.linkedIn}
           onChange={(e) => {
-            handleChangePersonalDetails('linkedIn', e.target.value);
+            handleChangePersonalDetails("linkedIn", e.target.value);
           }}
         />
-        <label htmlFor='github'>GitHub Link <span className="recommended">recommended</span></label>
-        <input 
-          id='github' 
-          type='text' 
+        <label htmlFor="github">
+          GitHub Link <span className="recommended">recommended</span>
+        </label>
+        <input
+          id="github"
+          type="text"
           value={personalDetails.gitHub}
           onChange={(e) => {
-            handleChangePersonalDetails('gitHub', e.target.value);
+            handleChangePersonalDetails("gitHub", e.target.value);
           }}
         />
       </div>
     </section>
-  )
+  );
 }
